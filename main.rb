ca84f058a5c5edd1b34dd562d622596e932b7059
Samir <samir1@brandeis.edu>
2015-04-29 17:12:30 -0400

    fix regex string match

--- main.rb
+++ main.rb
@@ -4,29 +4,13 @@ require 'open-uri'
 require 'json'
 require 'wikipedia'
 
-
-# actor = "Miles Teller"
-# page = Wikipedia.find(actor).content
-# # index = /([0-9][0-9][0-9][0-9][|][0-9]{1,2}[|][0-9]{1,2})/ =~ page
-# # page = page[index..index+9]
-# puts page
-
 def age_of actor
-	# page = Wikipedia.find(actor).content
-	# start_index = page.index('{{Birth date and age|')
-	# if start_index.nil?
-	# 	start_index = page.index('{{birth date and age|')
-	# end
-	# page = page[start_index..-1]
-	# end_index = page.index('}}')-1
-	# page = page[end_index-9..end_index]
 	page = Wikipedia.find(actor).content
-	index = /([0-9][0-9][0-9][0-9][|][0-9]{1,2}[|][0-9]{1,2})/ =~ page
-	page = page[index..index+9]
+	page = page.match(/([0-9][0-9][0-9][0-9][|][0-9]{1,2}[|][0-9]{1,2})/)[1]
 	page
 end
 
-Wolfram.appid = 'HU838J-6VWELQ7KA8'
+Wolfram.appid = 'KEY'
 
 query = 'movies in theaters'
 result = Wolfram.fetch(query)
@@ -35,7 +19,6 @@ hash = hash[:pods]["Result"][0].split("\n")
 hash.each do |line|
 	movie = line.split(" | ")[1]
 	puts movie
-	
 	movie_imdb  = open(URI.escape("http://www.omdbapi.com/?t=#{movie}&y=#{Time.new.year}&plot=short&r=json")) {|f| f.read }
 	parsed = JSON.parse(movie_imdb)
 	actors = parsed["Actors"].split(", ")
