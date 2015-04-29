4e47b58b294b45ebef79f447a5e66bdf1dcb431e
Samir <samir1@brandeis.edu>
2015-04-29 17:02:12 -0400

    remove test file

--- test.rb
+++
@@ -1,9 +0,0 @@
-require 'open-uri'
-require 'json'
-
-puts Time.new.year
-
-web_contents  = open('http://www.omdbapi.com/?t=Insurgent&y=&plot=short&r=json') {|f| f.read }
-parsed = JSON.parse(web_contents)
-actors = parsed["Actors"].split(", ")
-puts actors
